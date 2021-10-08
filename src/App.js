570aadbbfa14512eab63bec846be8ed15f46900b
Java Monk <javamonk.coder@gmail.com>
2021-10-08 15:30:27 +0530

    organised expenses components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 import './App.css'
 
 function App() {
+  const expenses = [
+    {title:'Car Insurance',date: new Date(),amount:'798.99'},
+    {title:'House Rent',date: new Date(),amount:'399.99'},
+    {title:'Laptop',date: new Date(),amount:'1298.99'},
+    {title:'Toilet Paper',date: new Date(),amount:'16.99'},
+]
   return (
     <div className="App">
-      <Expenses />
+      <Expenses items={expenses}/>
     </div>
   );
 }
