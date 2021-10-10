9beb75b61b3af497a55f61e982cd4e46fad33798
Java Monk <javamonk.coder@gmail.com>
2021-10-10 11:55:53 +0530

    implemented state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,26 @@
 import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css'
+import './ExpenseItem.css';
+import {useState} from 'react';
 
 function ExpenseItem(props) {
+
+    //using states to update title on click of button
+    //using array destructuring to separate values
+    // useState returns array of two values => 1.value , 2.function to change it
+    const [title,setTitle] = useState(props.title);
+
+    function clickHandler(){
+        //calling setTitle to update componenet
+        setTitle('Updated Title !');
+    }
+
+
     return (
         <div className='expenseItem'>
             <ExpenseDate date={props.date}/>
-            <div className='title'>{props.title}</div>
+            <div className='title'>{title}</div>
             <div className='amount'>${props.amount}</div>
+            <button className='btn' onClick={clickHandler}>Update!</button>
         </div>
     );
 }
