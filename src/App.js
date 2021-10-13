dd44b58d9f4c7427f3badfa08ad1ad3f283c8984
Java Monk <javamonk.coder@gmail.com>
2021-10-13 16:45:58 +0530

    added filteredYear components

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { useState } from 'react';
 
 
 const DUMMY_EXPENSES = [
-  {id:'e1',title:'Car Insurance',date: new Date(),amount:'798.99'},
-  {id:'e2',title:'House Rent',date: new Date(),amount:'399.99'},
+  {id:'e1',title:'Car Insurance',date: new Date('8/28/19'),amount:'798.99'},
+  {id:'e2',title:'House Rent',date: new Date('6/08/20'),amount:'399.99'},
   {id:'e3',title:'Laptop',date: new Date(),amount:'1298.99'},
   {id:'e4',title:'Toilet Paper',date: new Date(),amount:'16.99'},
 ];
@@ -17,7 +17,6 @@ function App() {
 const [expenses,setExpenses] = useState(DUMMY_EXPENSES);
 
 const addExpenseDataHandler = (expenseData) => {
-  console.log(expenseData);
   setExpenses((prevExpenses) => {
     return [expenseData,...prevExpenses];
   });
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,23 +11,40 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  //showing relevant components on filtering year
+  //.filter returns something for every element in array
+  const filteredExpenses = props.items.filter((expense) => {
+    let displayExpenses = expense.date.getFullYear().toString();
+    if (displayExpenses === filteredYear) {
+      return displayExpenses;
+    } else {
+      return false;
+    }
+  });
+
   return (
     <div className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterYearHandler}
       />
+      {/* if filtered expenses array does not have any elements , print error msg  */}
+      {filteredExpenses.length === 0 && (
+        <p className="no-expenses">Oops ! No Entry Found</p>
+        )}
+      {/* if filtered expenses array contains element , print it into dom */}
       {/* .map takes a function which is repeated for all elements in array */}
-      {props.items.map((elem) => {
-        return (
-          <ExpenseItem
-            key={elem.id}
-            title={elem.title}
-            amount={elem.amount}
-            date={elem.date}
-          />
-        );
-      })}
+      {filteredExpenses.length > 0 &&
+        filteredExpenses.map((elem) => {
+          return (
+            <ExpenseItem
+              key={elem.id}
+              title={elem.title}
+              amount={elem.amount}
+              date={elem.date}
+            />
+          );
+        })}
     </div>
   );
 }
